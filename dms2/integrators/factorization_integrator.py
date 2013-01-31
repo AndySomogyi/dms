2acdfef2fa4b388308fe6358ee40a7d1491b5aed
Andrew-AbiMansour <anabiman@indiana.edu>
2013-01-31 16:08:30 -0500

    modified integrator functions

--- dms2/integrators/factorization_integrator.py
+++ dms2/integrators/factorization_integrator.py
@@ -5,6 +5,20 @@ Created on Jan 30, 2013
 '''
 
 import integrator
+import dynamics
 
 class FactorizationIntegrator(integrator.Integrator):
-    pass
+    def cg_step(self):
+        """
+        perform a forward euler step (the most idiotic and unstable of all possible integrators)
+        to integrate the state variables. 
+        
+        reads self.cg_momenta to advance the cg state variables. 
+        
+        X[n+1] = X[n] + dt*dX/dt[n], and dX/dt is cg_moment/cg_mass.
+        """
+        
+        # forward euler 
+        cg_translate = self.system.dt * self.system.cg_moment
+        
+        self.system.translate(cg_translate)
