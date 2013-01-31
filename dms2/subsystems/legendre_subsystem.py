fad46d39ac492e1ede3a6094f70a6599b7715c6d
andy.somogyi <andy.somogyi@gmail.com>
2013-01-31 17:57:12 -0500

    added polynomial indices to Legendre Polynomials

--- dms2/subsystems/legendre_subsystem.py
+++ dms2/subsystems/legendre_subsystem.py
@@ -21,17 +21,19 @@ class LegendreSubsystem(subsystems.SubSystem):
     """
     A set of CG variables.
     """
-    def __init__(self, system, select):
+    def __init__(self, system, pindices, select):
         """
         Create a legendre subsystem.
         @param system: an object (typically the system this subsystem belongs to)
         which has the following attributes:
         box: an array describing the periodic boundary conditions.
+        @param pindices: a N*3 array of Legendre polynomial indices. 
         @param select: a select string used for Universe.selectAtoms which selects 
         the atoms that make up this subsystem.
         """
         self.system = system
         self.select = select
+        self.Indices = pindices
         
     def universe_changed(self, universe):
         self.atoms = universe.selectAtoms(self.select)
@@ -85,14 +87,14 @@ class LegendreSubsystem(subsystems.SubSystem):
         Constructs a matrix of orthonormalized legendre basis functions
         of size 3*Natoms x NCG 
         """ 
-        Indices = self.ComputeIndices(kmax)
+        
         Masses = np.reshape(self._Masses, [len(self._Masses), 1])
-        Basis = np.zeros([Scaled_Pos.shape[0], Indices.shape[0]],'f')
+        Basis = np.zeros([Scaled_Pos.shape[0], self.pindices.shape[0]],'f')
         
         for i in xrange(u.shape[1]):
-            px = legendre(indexes[i,0])(x)
-            py = legendre(indexes[i,1])(y)
-            pz = legendre(indexes[i,2])(z)
+            px = legendre(self.pindices[i,0])(x)
+            py = legendre(self.pindeces[i,1])(y)
+            pz = legendre(self.pindeces[i,2])(z)
             Basis[:,i] = px * py * pz
             
         WBasis = Basis * np.sqrt(Masses)
@@ -102,17 +104,56 @@ class LegendreSubsystem(subsystems.SubSystem):
         
         return WBasis
 
+def poly_indexes(psum):
+    """
+    Create 2D array of Legendre polynomial indices with index sum <= psum. 
 
-def LegendreSubsystemFactory(system, selects, *args): 
-    
+    For example, if psum is 1, the this returns
+    [[0, 0, 0],
+     [1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]]
+    Note, the sum of each row is less than or equal to 1.
+    """
+    a = []
+    for n in range(psum + 1):
+        for i in range(n+1):
+            for j in range(n+1-i):
+                a.append([n-i-j, j, i])
+    return np.array(a,'i')
+
+
+def LegendreSubsystemFactory(system, selects, *args):
+    """
+    create a list of LegendreSubsystems.
+
+    @param system
+    @param selects
+    @param args: a list of length 1 or 2. The first element is kmax.
+    """
+    kmax = 0
+    pindices = None
     if len(args) == 1:
+        kmax = int(args[0])
+    elif len(args) == 2:
+        kmax = int(args[0])
         toks = str(args[0]).split()
         if len(toks) == 2 and toks[0].lower() == "resid" and toks[1].lower() == "unique":
             groups = [system.universe.selectAtoms(s) for s in selects]
             resids = [resid for g in groups for resid in g.resids()]
             selects = ["resid " + str(resid) for resid in resids]
+    else:
+        raise ValueError("invalid args")
             
     # test to see if the generated selects work
     [system.universe.selectAtoms(select) for select in selects]
 
-    return (3, [LegendreSubsystem(system, select) for select in selects])
+    # create the polynomial indices
+    pindices = poly_indices(kmax)
+
+    # the number of CG variables
+    # actually, its sufficient to just say nrows * 3 as the 
+    # number of columns had better be 3.
+    ncg = pindices.shape[0] * pindices.shape[1]
+
+    return (ncg, [LegendreSubsystem(system, pindices, select) for select in selects])
