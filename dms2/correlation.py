ab5db2ef009349bc1d642972d4e80624cf775c72
Andrew-AbiMansour <anabiman@indiana.edu>
2013-02-03 00:38:51 -0500

    imported numpy

--- dms2/correlation.py
+++ dms2/correlation.py
@@ -8,6 +8,7 @@ from numpy import array, ndarray, arange, zeros, convolve, sum, std, mean, cumsu
 from random import randrange
 import h5py #@UnresolvedImport
 import pylab as p
+import numpy as np
 
 def correlation(x1, x2, window=None):
     """
