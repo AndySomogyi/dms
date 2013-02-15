f2ffe7e8855e2240e8e8f78dde7bac836ac45a24
andy.somogyi <andy.somogyi@gmail.com>
2013-02-15 10:37:04 -0500

    suppress eclipse squawking

--- dms2/cg.py
+++ dms2/cg.py
@@ -7,8 +7,8 @@ functions for calculating coarse grained variables
 '''
 
 from numpy import *
-import MDAnalysis as mda
-import h5py #@UnresolvedImport
+import MDAnalysis as mda            #@UnresolvedImport
+import h5py                         #@UnresolvedImport
 
 def mass_center_periodic(positions, masses, pbc):
     # make a column vector
